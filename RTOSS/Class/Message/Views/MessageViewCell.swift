49f7bbd95b537490455556e78ad0d322cc4ab661
xiaohuihui508 <15136718290@163.com>
2019-01-09 14:05:18 +0800

    cell新增

---
+++ RTOSS/Class/Message/Views/MessageViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageViewCell.swift
+//  RTOSS
+//
+//  Created by isoft on 2019/1/9.
+//  Copyright © 2019 isoft. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
